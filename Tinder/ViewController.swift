554943da7658558ca8ea33b664ba5e4102a5fc24
Nicholas Wong <nic.wzx@gmail.com>
2019-09-20 16:16:53 -0700

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Nicholas Wong on 9/20/19.
+//  Copyright © 2019 Nicholas Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
